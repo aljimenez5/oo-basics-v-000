87e688438db94d7b678961a774a2a3c828295810
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-11 23:12:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,13 +7,13 @@ class Book
   def title
     @title
   end
-
-  def author
-    @author = author
-  end
   
   def author=(author)
     @author = author
+  end
+
+  def author
+    @author 
   end
 
 end
